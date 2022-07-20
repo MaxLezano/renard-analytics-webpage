d08a1dab4f4038ebb0c31db035088c02dab5f0d4
Facundo Esquivel Fagiani <facufref@gmail.com>
2022-07-20 18:02:24 -0300

    added MainBanner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Footer from "./components/Footer/Footer";
+import MainBanner from "./components/MainBanner/MainBanner";
 
 function App() {
   return (
     <div>
+      <MainBanner />
       <Footer/>
     </div>
   );
