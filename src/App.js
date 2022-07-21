3bc90bfa16642ab7c7088cd089cf8c3aced1bcdf
German Vera <germanevera@gmail.com>
2022-07-21 13:15:00 -0300

    fixing some component rendering order

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Team from "./components/Main/Team";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import MainBanner from "./components/MainBanner/MainBanner";
+import Navbar from "./components/Header/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <MainBanner />
       <Team />
       <Services />
       <Experience />
       <Contact />
       <Header />
-      <MainBanner />
       <Footer />
     </div>
   );
