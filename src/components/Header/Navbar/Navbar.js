75791b11d6ba9d55dff7dd7d0dd839ae68d856a4
Max Lezano <david.lezano@gmail.com>
2022-07-21 12:25:29 -0300

    add header to main banner

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import logoImage from '../../../assets/images/logo-white.png';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className='container-fluid col-10 py-4'>
-      <div className='row justify-content-between'>
-        <img className='col-3' src={logoImage} alt="logo" />
-        <div className='col-6 d-flex justify-content-end align-items-center'>
-          <a className='text-decoration-none text-white p-3' href="/">¿Quienes Somos?</a>
-          <a className='text-decoration-none text-white p-3' href="/">Blog</a>
-          <button type="button" class="btn btn-warning my-3 ms-3">Contratar</button>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container-fluid py-3 col-lg-9">
+        <img className='navbar-brand col-9 col-lg-2' src={logoImage} alt="logo"/>
+        <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse bgBurguer" id="navbarNavAltMarkup">
+          <div className="navbar-nav align-items-center contTextNav">
+            <a className="nav-link textNav" aria-current="page" href="/#">¿Quienes Somos?</a>
+            <a className="nav-link textNav" href="/#">Blog</a>
+          </div>
+          <button type="button" className="btn btn-danger my-3 px-5">Contratar</button>
         </div>
       </div>
-    </div>
-  )
+    </nav>
+  );
 }
 
 export default Navbar;
