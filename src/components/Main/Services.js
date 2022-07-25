15237b521b470b16accfe50a08b2f45facfd34f1
Max Lezano <david.lezano@gmail.com>
2022-07-25 11:45:07 -0300

    change dataRenard

--- src/components/Main/Services.js
+++ src/components/Main/Services.js
@@ -1,32 +1,6 @@
 import React from "react";
 import Card from "../Card/Card";
-
-const services = [
-  {
-    image: "",
-    title: "Integracion de datos",
-    description:
-      "Integramos datos de distintas fuentes de forma rápida y eficaz utilizando las más recientes técnicas de ingeniería de datos.",
-  },
-  {
-    imagen: "",
-    title: "Análisis deportivo",
-    description:
-      "Analizamos datos cuantitativos y cualitativos para proveer completos informes para la toma de decisiones deportivas, referentes a estrategias, preparación, entrenamientos y más.",
-  },
-  {
-    imagen: "",
-    title: "Scouting de Jugadores",
-    description:
-      "Proveemos una búsqueda completa y exhaustiva de jugadores según características específicas, rendimiento, proyección a futuro, similaridad y más.",
-  },
-  {
-    imagen: "",
-    title: "Asesoría y consultoría",
-    description:
-      "Asesoramos en la implementación de nuevas tecnologías para el deporte, guiando en el proceso y asegurando que tu inversión tenga el mejor retorno posible.",
-  },
-];
+import { services } from "../../dataRenard";
 
 const Services = () => {
   return (
