29275d91cc8c2d267f53340b82324afad346cb4b
Max Lezano <david.lezano@gmail.com>
2022-07-25 13:10:02 -0300

    added class for blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -5,7 +5,7 @@ import Header from '../components/Header/Header'
 const Blog = () => {
   return (
     <>
-      <Header />
+      <Header className={'bgPages'}/>
       <div>
         Aquí va el blog
       </div>
