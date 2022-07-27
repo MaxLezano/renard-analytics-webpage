d92359ecc96bf3b7d902f07f526b90d70f61d61d
Marcos Cortez <mdavidcortez@gmail.com>
2022-07-27 15:50:54 -0300

    add css in main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import MainBanner from "../MainBanner/MainBanner";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
+import Teamworkimg from "../../assets/images/stats-illustration.svg"
 import Card from "../Card/Card";
 import { services } from "../../dataRenard";
 import emailImg from "../../assets/images/email-illustration.svg";
@@ -12,32 +13,38 @@ const Main = () => {
     <div>
       <MainBanner />
       <div className="container-fluid m-5 text-center">
-        <section className="justify-content-center">
-          <img alt="grafica de algo" />
-          <p>Trabajo en progreso</p>
-          <h3>Un equipo experimentado, juntos desde 2021.</h3>
-          <p>
+        <div className="row d-flex justify-content-center">
+        <section className="col-4 justify-content-center">
+          <p className="parrafoStyle">Trabajo en progreso</p>
+          <h3 className="h3Style">Un equipo experimentado, juntos desde 2021.</h3>
+          <p className="text-white">
             Somos un equipo de experimentados analistas e ingenieros de datos.
             Nos unimos hace poco, pero ya logramos mucho.
           </p>
           <table className="container-fluid text-center">
-            <thead className="row d-flex">
-              <tr>
-                <th>Soluciones completadas</th>
-                <th>En Desarrollo</th>
+            <thead>
+              <tr className="row d-flex justify-content-center">
+                <th className="col-4 text-white">Soluciones completadas</th>
+                <th className="col-4 text-white">En Desarrollo</th>
               </tr>
             </thead>
             <tbody className="container-fluid text-center">
-              <tr className="row d-flex">
-                <td>4+</td>
-                <td>10+</td>
+              <tr className="row d-flex justify-content-center">
+                <td className="col-4 text-white fw-bold">4+</td>
+                <td className="col-4 text-white fw-bold">10+</td>
               </tr>
             </tbody>
           </table>
           <Link to="/about">
-            <Button description={"Ver Mas"} />
+            <Button
+              className={"btn btn-danger"} 
+              description={"Ver Mas"} />
           </Link>
         </section>
+        <section className="col-4">
+          <img className="imgSize" src={Teamworkimg} alt="grafica de algo" />
+        </section>
+        </div>
 
         <div>
           <section>
@@ -49,6 +56,7 @@ const Main = () => {
             </div>
           </section>
         </div>
+        
         <div className="container-fluid text-center my-5">
           <div className="row d-flex justify-content-center align-items-center">
             <section className="col-4">
@@ -63,7 +71,7 @@ const Main = () => {
               <h3 className="h3Style">
                 Un equipo de profesionales con experiencia.
               </h3>
-              <p>
+              <p className="text-white">
                 Contamos con un equipo de experimentados ingenieros al corriente
                 con un gran abanico de tecnologías.
               </p>
@@ -76,7 +84,7 @@ const Main = () => {
             <section className="col-4">
               <p className="parrafoStyle">Contactanos</p>
               <h3 className="h3Style">Todas sus consultas son bienvenidas!</h3>
-              <p>
+              <p className="text-white">
                 Estamos a su disposición por cualquier consulta via correo
                 electrónico o través de cualquiera de nuestras redes sociales.
               </p>
